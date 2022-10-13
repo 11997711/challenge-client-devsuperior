62ea868821ac5b90d492d7610462ab0ef5966416
Andre Matos <andreluisv8@gmail.com>
2022-10-13 16:46:01 -0300

    Project Challenge Client Created

---
+++ backend-api-crud-clientes/src/test/java/com/desafio/crud/clientes/ApiCrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.crud.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
